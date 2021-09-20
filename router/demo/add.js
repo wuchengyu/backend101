cf446f26a97c572815a52538f730d089bababe7b
Jamie Wu <jvyu59jvyu@icloud.com>
2021-09-20 17:19:27 +0800

    feat: 調整 swagger

--- router/demo/add.js
+++ router/demo/add.js
@@ -5,6 +5,7 @@ const { add } = require('core/demo');
 const router = express.Router();
 
 router.get('/:a(\\d+)/:b(\\d+)', (req, res) => {
+  // #swagger.tags = ['Demo']
   const { a, b } = req.params;
   const data = add(+a, +b);
   return res.json({
--- scripts/generate-swagger.js
+++ scripts/generate-swagger.js
@@ -13,46 +13,19 @@ const doc = {
   produces: ['application/json'],
   tags: [
     {
-      name: 'User',
-      description: 'Endpoints',
+      name: 'Demo',
+      description: 'Demo',
     },
   ],
   securityDefinitions: {
-    apiKeyAuth: {
+    bearerAuth: {
+      name: 'Authorization', // name of the header, query parameter or cookie
       type: 'apiKey',
       in: 'header', // can be "header", "query" or "cookie"
-      name: 'X-API-KEY', // name of the header, query parameter or cookie
-      description: 'any description...',
+      description: 'Enter JWT Bearer Token.',
     },
   },
   definitions: {
-    Parents: {
-      father: 'Simon Doe',
-      mother: 'Marie Doe',
-    },
-    User: {
-      name: 'Jhon Doe',
-      age: 29,
-      parents: {
-        $ref: '#/definitions/Parents',
-      },
-      diplomas: [
-        {
-          school: 'XYZ University',
-          year: 2020,
-          completed: true,
-          internship: {
-            hours: 290,
-            location: 'XYZ Company',
-          },
-        },
-      ],
-    },
-    AddUser: {
-      $name: 'Jhon Doe',
-      $age: 29,
-      about: '',
-    },
   },
 };
 
