16f99e2c94c4e02ac41a79b28569d27d6ab4dd11
Jamie Wu <jvyu59jvyu@icloud.com>
2021-09-20 14:23:34 +0800

    feat: 加入測試
    - jest

---
+++ core/demo/__tests__/index.js
@@ -0,0 +1,11 @@
+const demo = require('core/demo/index')
+
+describe('#Demo Testing', () => {
+  describe('Add', () => {
+    test('1 + 1 should eqaul 2', () => {
+      const a = 1
+      const b = 1
+      expect(demo.add(a, b)).toBe(2)
+    });
+  });
+});
